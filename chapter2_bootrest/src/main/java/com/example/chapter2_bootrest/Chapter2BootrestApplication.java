b1671d10e7dc1b9a97d86efbcf40ca88ee75bf82
youngran.kwon <youngran.kwon@lemonhc.com>
2021-11-01 16:57:54 +0900

    11/1-(2)

--- chapter2_bootrest/src/main/java/com/example/chapter2_bootrest/Chapter2BootrestApplication.java
+++ chapter2_bootrest/src/main/java/com/example/chapter2_bootrest/Chapter2BootrestApplication.java
@@ -1,7 +1,11 @@
 package com.example.chapter2_bootrest;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,18 +18,30 @@ public class Chapter2BootrestApplication {
 
 }
 
+@Slf4j
 @RestController
 class GreetingController {
 
-    @RequestMapping("/")
+    @Autowired
+    Environment environment;
+
     Greet greet() {
+        log.info("bootrest.customproperty " + environment.getProperty("bootrest.customproperty"));
         return new Greet("Hello World!");
+    }
+
+    @RequestMapping("/")
+    Greet sayHello() {
+        return greet();
     }
 }
 
 class Greet {
     private String message;
-    public Greet(){}
+
+    public Greet() {
+    }
+
     public Greet(String message) {
         this.message = message;
     }
--- chapter2_bootrest/src/test/java/com/example/chapter2_bootrest/GreetTest.java
+++ chapter2_bootrest/src/test/java/com/example/chapter2_bootrest/GreetTest.java
@@ -4,8 +4,15 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.codec.Base64;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.StandardCharsets;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = GreetTest.class)
@@ -16,5 +23,16 @@ class GreetTest {
         RestTemplate restTemplate = new RestTemplate();
         Greet greet = restTemplate.getForObject("http://localhost:8080", Greet.class);
         Assert.assertEquals("Hello World!", greet.getMessage());
+    }
+
+    @Test
+    public void testSecureService() {
+        String plainCreds = "guest:guest123";
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization", "Basic " + new String(Base64.encode(plainCreds.getBytes())));
+        HttpEntity<String> request = new HttpEntity<String>(httpHeaders);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Greet> response = restTemplate.exchange("http://localhost:9080", HttpMethod.GET, request, Greet.class);
+        Assert.assertEquals("Hello World!", response.getBody().getMessage());
     }
 }
