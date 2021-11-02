90d4a4e46e8aaf1acc136f23c54feab05537561f
youngran.kwon <youngran.kwon@lemonhc.com>
2021-11-02 16:50:41 +0900

    11/2

--- chapter2_bootrest/src/main/java/com/example/chapter2_bootrest/Chapter2BootrestApplication.java
+++ chapter2_bootrest/src/main/java/com/example/chapter2_bootrest/Chapter2BootrestApplication.java
@@ -1,14 +1,18 @@
 package com.example.chapter2_bootrest;
 
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableResourceServer
+@EnableAuthorizationServer
 @SpringBootApplication
 public class Chapter2BootrestApplication {
 
@@ -18,15 +22,16 @@ public class Chapter2BootrestApplication {
 
 }
 
-@Slf4j
 @RestController
 class GreetingController {
+
+    private static Logger logger = LoggerFactory.getLogger(GreetingController.class);
 
     @Autowired
     Environment environment;
 
     Greet greet() {
-        log.info("bootrest.customproperty " + environment.getProperty("bootrest.customproperty"));
+        logger.info("bootrest.customproperty " + environment.getProperty("bootrest.customproperty"));
         return new Greet("Hello World!");
     }
 
--- chapter2_bootrest/src/test/java/com/example/chapter2_bootrest/Chapter2BootrestApplicationTests.java
+++ chapter2_bootrest/src/test/java/com/example/chapter2_bootrest/Chapter2BootrestApplicationTests.java
@@ -1,13 +1,65 @@
 package com.example.chapter2_bootrest;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.codec.Base64;
+import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
-class Chapter2BootrestApplicationTests {
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Chapter2BootrestApplication.class)
+@WebIntegrationTest
+public class Chapter2BootrestApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void testVanillaService() {
+        RestTemplate restTemplate = new RestTemplate();
+        Greet greet = restTemplate.getForObject("http://localhost:8080", Greet.class);
+        Assert.assertEquals("Hello World!", greet.getMessage());
+    }
+
+    /**
+     * spring-security 적용
+     */
+    @Test
+    public void testSecureService() {
+        String plainCreds = "guest:guest123";
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization", "Basic " + new String(Base64.encode(plainCreds.getBytes())));
+        HttpEntity<String> request = new HttpEntity<String>(httpHeaders);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Greet> response = restTemplate.exchange("http://localhost:8080", HttpMethod.GET, request, Greet.class);
+        Assert.assertEquals("Hello World!", response.getBody().getMessage());
+    }
+
+    /**
+     * spring-security-oauth2 적용(spring version 1.3.5.RELEASE 에서만 동작)
+     */
+    @Test
+    public void testOAuthService() {
+        ResourceOwnerPasswordResourceDetails resource = new ResourceOwnerPasswordResourceDetails();
+        resource.setUsername("guest");
+        resource.setPassword("guest123");
+        resource.setAccessTokenUri("http://localhost:8080/oauth/token");
+        resource.setClientId("trustedclient");
+        resource.setClientSecret("trustedclient123");
+        resource.setGrantType("password");
+
+        DefaultOAuth2ClientContext clientContext = new DefaultOAuth2ClientContext();
+        OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(resource, clientContext);
+
+        Greet greet = restTemplate.getForObject("http://localhost:8080", Greet.class);
+        Assert.assertEquals("Hello World!", greet.getMessage());
     }
 
 }
