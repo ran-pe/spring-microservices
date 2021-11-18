288045442881be745533eaa266c71ca17ea8f2ce
youngran.kwon <youngran.kwon@lemonhc.com>
2021-11-18 16:04:07 +0900

    11/18

--- spring-boot-jpa/src/main/java/me/ran/Application.java
+++ spring-boot-jpa/src/main/java/me/ran/Application.java
@@ -2,17 +2,24 @@ package me.ran;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Application {
 
     public static void main(String[] args) throws SQLException {
         String url = "jdbc:postgresql://localhost:5432/springdata";
-        String username = "youngran";
-        String password = "pass";
+        String username = "ranran";
+        String password = "ranrantest09";
 
         try(Connection connection = DriverManager.getConnection(url, username, password)) {
             System.out.println("Connection created: " + connection);
+//            String sql = "create table account (id int, username varchar(256), password varchar(256));";
+            String sql = "insert into account values (1, 'youngran', 'youngransecret');";
+            try(PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                preparedStatement.execute();
+            }
+
         }
 
     }
